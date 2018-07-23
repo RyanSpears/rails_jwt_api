dd64f55cf44fd8ca00e459652f574e33bdfbc795
Ryan Spears <sperj001@hotmail.com>
2018-07-23 15:53:35 +0930

    Initial Commit

---
+++ config/application.rb
@@ -0,0 +1,40 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module JwtApi
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.autoload_paths << Rails.root.join('lib')
+
+    # This should be set to <%= ENV['RAILS_DB_PWD'] %>
+    config.secret_key_base = 'bec3eee2bbf1d68c69e140cccfe9dd7889fa71fc226ae37ee4dc712e2c6972b9a460e9565fa6ac13f2bbdd908b7490ddf4a081c19a3bd0d928cbced5f46cfd05'
+  end
+end
